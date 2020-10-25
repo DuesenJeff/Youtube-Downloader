7e888d132f4a581df2311da2351df6291b253351
DuesenJeff <jeffrey.burch@outlook.com>
2020-10-25 17:05:46 +0100

    Basically wrote this in one go

--- Youtube_Downloader/Form1.Designer.cs
+++ Youtube_Downloader/Form1.Designer.cs
@@ -28,13 +28,149 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.LinkLabel = new System.Windows.Forms.Label();
+            this.YoutubeLinkBox = new System.Windows.Forms.TextBox();
+            this.RetrieveButton = new System.Windows.Forms.Button();
+            this.FormatLabel = new System.Windows.Forms.Label();
+            this.FormatDropDown = new System.Windows.Forms.ComboBox();
+            this.ResolutionLabel = new System.Windows.Forms.Label();
+            this.ResDropDown = new System.Windows.Forms.ComboBox();
+            this.FileNameLabel = new System.Windows.Forms.Label();
+            this.FileNameBox = new System.Windows.Forms.TextBox();
+            this.DownloadButton = new System.Windows.Forms.Button();
+            this.VideoLocation = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // LinkLabel
+            // 
+            this.LinkLabel.AutoSize = true;
+            this.LinkLabel.Location = new System.Drawing.Point(36, 41);
+            this.LinkLabel.Name = "LinkLabel";
+            this.LinkLabel.Size = new System.Drawing.Size(73, 13);
+            this.LinkLabel.TabIndex = 0;
+            this.LinkLabel.Text = "Youtube Link:";
+            // 
+            // YoutubeLinkBox
+            // 
+            this.YoutubeLinkBox.Location = new System.Drawing.Point(39, 57);
+            this.YoutubeLinkBox.Name = "YoutubeLinkBox";
+            this.YoutubeLinkBox.Size = new System.Drawing.Size(423, 20);
+            this.YoutubeLinkBox.TabIndex = 1;
+            // 
+            // RetrieveButton
+            // 
+            this.RetrieveButton.Location = new System.Drawing.Point(477, 57);
+            this.RetrieveButton.Name = "RetrieveButton";
+            this.RetrieveButton.Size = new System.Drawing.Size(114, 21);
+            this.RetrieveButton.TabIndex = 2;
+            this.RetrieveButton.Text = "Retrieve Link";
+            this.RetrieveButton.UseVisualStyleBackColor = true;
+            this.RetrieveButton.Click += new System.EventHandler(this.RetrieveButton_Click);
+            // 
+            // FormatLabel
+            // 
+            this.FormatLabel.AutoSize = true;
+            this.FormatLabel.Location = new System.Drawing.Point(36, 203);
+            this.FormatLabel.Name = "FormatLabel";
+            this.FormatLabel.Size = new System.Drawing.Size(61, 13);
+            this.FormatLabel.TabIndex = 3;
+            this.FormatLabel.Text = "File Format:";
+            // 
+            // FormatDropDown
+            // 
+            this.FormatDropDown.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.FormatDropDown.FormattingEnabled = true;
+            this.FormatDropDown.Location = new System.Drawing.Point(39, 219);
+            this.FormatDropDown.Name = "FormatDropDown";
+            this.FormatDropDown.Size = new System.Drawing.Size(70, 21);
+            this.FormatDropDown.TabIndex = 4;
+            this.FormatDropDown.SelectedIndexChanged += new System.EventHandler(this.FormatDropDown_SelectedIndexChanged);
+            // 
+            // ResolutionLabel
+            // 
+            this.ResolutionLabel.AutoSize = true;
+            this.ResolutionLabel.Location = new System.Drawing.Point(36, 255);
+            this.ResolutionLabel.Name = "ResolutionLabel";
+            this.ResolutionLabel.Size = new System.Drawing.Size(60, 13);
+            this.ResolutionLabel.TabIndex = 5;
+            this.ResolutionLabel.Text = "Resolution:";
+            // 
+            // ResDropDown
+            // 
+            this.ResDropDown.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ResDropDown.FormattingEnabled = true;
+            this.ResDropDown.Location = new System.Drawing.Point(39, 271);
+            this.ResDropDown.Name = "ResDropDown";
+            this.ResDropDown.Size = new System.Drawing.Size(121, 21);
+            this.ResDropDown.TabIndex = 6;
+            this.ResDropDown.SelectedIndexChanged += new System.EventHandler(this.ResDropDown_SelectedIndexChanged);
+            // 
+            // FileNameLabel
+            // 
+            this.FileNameLabel.AutoSize = true;
+            this.FileNameLabel.Location = new System.Drawing.Point(36, 312);
+            this.FileNameLabel.Name = "FileNameLabel";
+            this.FileNameLabel.Size = new System.Drawing.Size(57, 13);
+            this.FileNameLabel.TabIndex = 7;
+            this.FileNameLabel.Text = "File Name:";
+            // 
+            // FileNameBox
+            // 
+            this.FileNameBox.Location = new System.Drawing.Point(39, 328);
+            this.FileNameBox.Name = "FileNameBox";
+            this.FileNameBox.Size = new System.Drawing.Size(362, 20);
+            this.FileNameBox.TabIndex = 8;
+            // 
+            // DownloadButton
+            // 
+            this.DownloadButton.Enabled = false;
+            this.DownloadButton.Location = new System.Drawing.Point(39, 354);
+            this.DownloadButton.Name = "DownloadButton";
+            this.DownloadButton.Size = new System.Drawing.Size(362, 23);
+            this.DownloadButton.TabIndex = 9;
+            this.DownloadButton.Text = "Download";
+            this.DownloadButton.UseVisualStyleBackColor = true;
+            this.DownloadButton.Click += new System.EventHandler(this.DownloadButton_Click);
+            // 
+            // VideoLocation
+            // 
+            this.VideoLocation.FileOk += new System.ComponentModel.CancelEventHandler(this.VideoLocation_FileOk);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(861, 487);
+            this.Controls.Add(this.DownloadButton);
+            this.Controls.Add(this.FileNameBox);
+            this.Controls.Add(this.FileNameLabel);
+            this.Controls.Add(this.ResDropDown);
+            this.Controls.Add(this.ResolutionLabel);
+            this.Controls.Add(this.FormatDropDown);
+            this.Controls.Add(this.FormatLabel);
+            this.Controls.Add(this.RetrieveButton);
+            this.Controls.Add(this.YoutubeLinkBox);
+            this.Controls.Add(this.LinkLabel);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label LinkLabel;
+        private System.Windows.Forms.TextBox YoutubeLinkBox;
+        private System.Windows.Forms.Button RetrieveButton;
+        private System.Windows.Forms.Label FormatLabel;
+        private System.Windows.Forms.ComboBox FormatDropDown;
+        private System.Windows.Forms.Label ResolutionLabel;
+        private System.Windows.Forms.ComboBox ResDropDown;
+        private System.Windows.Forms.Label FileNameLabel;
+        private System.Windows.Forms.TextBox FileNameBox;
+        private System.Windows.Forms.Button DownloadButton;
+        private System.Windows.Forms.SaveFileDialog VideoLocation;
     }
 }
 
--- Youtube_Downloader/Form1.cs
+++ Youtube_Downloader/Form1.cs
@@ -10,6 +10,8 @@ using System.Windows.Forms;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.IO;
+using System.Diagnostics;
+using System.Reflection;
 
 namespace Youtube_Downloader
 {
@@ -17,16 +19,177 @@ namespace Youtube_Downloader
     {
         public Video video;
 
+        public string currentLink;
+
+        public string selectedFormat;
+
+        public string pythonPath;
+
+        List<Stream> videoList;
+
+        List<Stream> audioList;
+
+        string videoLocation;
+
+        bool isVideo;
+
         public Form1()
         {
             InitializeComponent();
-            InitializeJSON();
         }
 
         public void InitializeJSON()
         {
             string jsonString = File.ReadAllText("./scripts/output.json");
             video = JsonSerializer.Deserialize<Video>(jsonString);
+        }
+
+        private void RetrieveButton_Click(object sender, EventArgs e)
+        {
+            currentLink = YoutubeLinkBox.Text;
+
+            this.FormatDropDown.Items.AddRange(new object[] { ".avi", ".flv", ".mov", ".mp4", ".mp3", ".ogg", ".wav", ".webm" });
+
+            ExecutePython();
+
+            InitializeJSON();
+
+            this.FileNameBox.Text = string.Format("{0} - {1}", video.Name, video.Uploader);
+
+            ExtractResolutionAndBitrate();
+        }
+
+        private void AddDropDownItems()
+        {
+            this.ResDropDown.Items.Clear();
+            if(selectedFormat == ".mp3" || selectedFormat == ".wav" || selectedFormat == ".ogg")
+            {
+                this.ResolutionLabel.Text = "Bitrate:";
+                foreach(Stream s in audioList)
+                {
+                    isVideo = false;
+                    this.ResDropDown.Items.Add(string.Format("{0}@{1}", s.FileFormat, s.Resolution));
+                }
+            } else
+            {
+                this.ResolutionLabel.Text = "Resolution:";
+                foreach (Stream s in videoList)
+                {
+                    isVideo = true;
+                    this.ResDropDown.Items.Add(string.Format("{0}@{1}", s.FileFormat, s.Resolution));
+                }
+            }
+        }
+
+        private void FormatDropDown_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            selectedFormat = (string)FormatDropDown.SelectedItem;
+            AddDropDownItems();
+        }
+
+        private void ExecutePython()
+        {
+            ProcessStartInfo info = new ProcessStartInfo();
+            info.FileName = "python";
+            info.Arguments = string.Format(@"./scripts/youtube.py ""{0}""", currentLink);
+            info.UseShellExecute = false;
+            info.RedirectStandardOutput = true;
+            info.WorkingDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            Process p = Process.Start(info);
+            StreamReader reader = p.StandardOutput;
+            string result = reader.ReadToEnd();
+            p.WaitForExit();
+        }
+
+        private void ExtractResolutionAndBitrate()
+        {
+            videoList = new List<Stream>();
+            audioList = new List<Stream>();
+
+            foreach(Stream s in video.Streams)
+            {
+                if(s.StreamType == "audio")
+                {
+                    audioList.Add(s);
+                } else if(s.StreamType == "video")
+                {
+                    videoList.Add(s);
+                }
+            }
+        }
+
+        private void DownloadButton_Click(object sender, EventArgs e)
+        {
+            if (isVideo)
+            {
+                videoLocation = videoList[ResDropDown.SelectedIndex].Link;
+            } else
+            {
+                videoLocation = audioList[ResDropDown.SelectedIndex].Link;
+            }
+
+            VideoLocation.FileName = FileNameBox.Text+FormatDropDown.SelectedItem;
+            VideoLocation.ShowDialog();
+        }
+
+        private void VideoLocation_FileOk(object sender, CancelEventArgs e)
+        {
+            string path = Path.GetDirectoryName(VideoLocation.FileName);
+            DownloadButton.Enabled = false;
+            ExecuteFFMPEG(path);
+        }
+
+        private void ResDropDown_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            DownloadButton.Enabled = true;
+        }
+
+        private void ExecuteFFMPEG(string path)
+        {
+            if (isVideo)
+            {
+                ProcessStartInfo info = new ProcessStartInfo();
+                info.FileName = "./ffmpeg/bin/ffmpeg.exe";
+                info.Arguments = string.Format(@"-i ""{0}"" -y ""{1}""",videoLocation , "vid" + (string)FormatDropDown.SelectedItem);
+                info.UseShellExecute = false;
+                info.RedirectStandardOutput = true;
+                info.WorkingDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+                Process p = Process.Start(info);
+                StreamReader reader = p.StandardOutput;
+                p.WaitForExit();
+
+                ProcessStartInfo info1 = new ProcessStartInfo();
+                info1.FileName = "./ffmpeg/bin/ffmpeg.exe";
+                info1.Arguments = string.Format(@"-i ""{0}"" -y ""{1}""", audioList.Last().Link, "aud.mp3");
+                info1.UseShellExecute = false;
+                info1.RedirectStandardOutput = true;
+                info1.WorkingDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+                Process p1 = Process.Start(info1);
+                StreamReader reader1 = p1.StandardOutput;
+                p1.WaitForExit();
+
+                ProcessStartInfo info2 = new ProcessStartInfo();
+                info2.FileName = "./ffmpeg/bin/ffmpeg.exe";
+                info2.Arguments = string.Format(@"-y -i {0} -i aud.mp3 -c copy ""{1}""", "vid" + (string)FormatDropDown.SelectedItem, VideoLocation.FileName.Replace("\\", "/"));
+                info2.UseShellExecute = false;
+                Process p2 = Process.Start(info2);
+                p2.WaitForExit();
+
+                File.Delete("vid" + (string)FormatDropDown.SelectedItem);
+                File.Delete("aud.mp3");
+            }
+            else
+            {
+                ProcessStartInfo info = new ProcessStartInfo();
+                info.FileName = "./ffmpeg/bin/ffmpeg.exe";
+                info.Arguments = string.Format(@"-i ""{0}"" -y ""{1}""", videoLocation, VideoLocation.FileName.Replace("\\", "/"));
+                info.UseShellExecute = false;
+                info.RedirectStandardOutput = true;
+                info.WorkingDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+                Process p = Process.Start(info);
+                StreamReader reader = p.StandardOutput;
+                p.WaitForExit();
+            }
         }
     }
 }
